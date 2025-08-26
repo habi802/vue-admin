6a16f412c43ab4958b38ad881e5f28b7d4a54843
habi802 <shb8002@gmail.com>
2025-08-26 15:26:34 +0900

    [feat] 헤더, 사이드바, 폰트 세팅

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    ㅎㅇ
+    
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <span>관리자</span> 님, 환영합니다!
+    </header>
+</template>
+
+<style scoped>
+
+</style>
