5965461386c8b02701f93f152fadc62ae8027930
habi802 <shb8002@gmail.com>
2025-08-27 14:41:43 +0900

    [feat] 사이드바 작업 완료

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,7 +5,7 @@
 <template>
     <!-- 햄버거 버튼 (작은 화면에서만) -->
     <div>
-        <button class="btn btn-outline-dark d-md-none m-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">☰</button>
+        <button class="hamburger btn btn-outline-dark d-md-none m-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">☰</button>
     </div>
 
     <!-- 사이드바 -->
@@ -15,21 +15,27 @@
         </div>
         <ul class="nav nav-pills flex-column">
             <li class="nav-item"><a href="#" class="nav-link active">대시보드</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">통계</a></li>
-            <li class="nav-item">
-                <a class="nav-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#submenu1" role="button" aria-expanded="false" aria-controls="submenu1">
-                    메뉴 1
-                </a>
-                <ul class="collapse nav flex-column ms-3" id="submenu1">
-                    <li class="nav-item"><a class="nav-link" href="#">하위 1-1</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">하위 1-2</a></li>
-                </ul>
-            </li>
+            <li class="nav-item"><a href="#" class="nav-link">회원 관리</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">가게 관리</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">리뷰 관리</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">주문 관리</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">결제 관리</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">고객 문의</a></li>
         </ul>
     </div>
 </template>
 
 <style scoped>
+.hamburger {
+    border-color: #FF6666;
+    color: #FF6666;
+
+    &:hover {
+        background-color: #FF6666;
+        color: white;
+    }
+}
+
 #sidebar {
     width: 220px;
 }
@@ -37,5 +43,14 @@
 .logo {
     width: 75px;
     height: 75px;
+}
+
+.nav-link {
+    color: #E65558;
+}
+
+.nav-link.active {
+    background-color: #E65558;
+    color: white;
 }
 </style>
